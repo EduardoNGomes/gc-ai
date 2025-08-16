bad786952dacb40a8db3ed11e0b8508bd726f4a3
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-08-16 13:04:41 -0300

    feat:implement errors package

--- errs/main.go
+++ errs/main.go
@@ -6,4 +6,8 @@ const CannotOpenFileErr = "Can not open file"
 
 const UnexpectedError = "Unexpected error"
 
+const ErrorOnConvertDataToByte = "Error on convert data to byte"
+
+const ErrorOnWriteFileConfig = "Error on write file config"
+
 var InvalidJson *json.UnmarshalTypeError
