c8374059b62b2be8b1844bb07c0821cebae2d03e
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-09-15 21:49:52 -0300

    feat(agents): implement Gemini agent for commit generation

--- internal/agents/agent.interface.go
+++ internal/agents/agent.interface.go
@@ -6,7 +6,9 @@ import (
 
 const Prompt = "You are a senior software engineer. Analyze the given git diff and output only a single-line, professional conventional commit message following the Conventional Commits specification (type, scope if applicable, and short description): \n"
 
+type Agent struct{}
+
 type AgentMethods interface {
 	GetCommit(c *c.Config, diff string) (string, error)
-	Edit(v string)
+	Edit(v string) string
 }
---
+++ internal/agents/gemini.agent.go
@@ -0,0 +1,46 @@
+package agents
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/eduardongomes/gcai/internal/config"
+	"google.golang.org/genai"
+)
+
+type GeminiAgent struct{}
+
+func (agent *GeminiAgent) GetCommit(config *config.Config, diff string) (string, error) {
+	key := config.GetGeminiKey()
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, &genai.ClientConfig{
+		APIKey:  key,
+		Backend: genai.BackendGeminiAPI,
+	})
+
+	if err != nil {
+		return "", fmt.Errorf("Error on get context: %w", err)
+	}
+
+	result, err := client.Models.GenerateContent(
+		ctx,
+		"gemini-2.5-flash-lite",
+		genai.Text(Prompt+diff),
+		nil,
+	)
+
+	if err != nil {
+		return "", fmt.Errorf("error on generate content %w", err)
+	}
+
+	return result.Text(), nil
+}
+
+func (agent *GeminiAgent) Edit(v string) string {
+	return ""
+}
+
+func NewGeminiAgent() *GeminiAgent {
+	return &GeminiAgent{}
+}
