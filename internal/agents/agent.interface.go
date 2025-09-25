074af3d61e63ec0656328d2597426bcb2f094bb9
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-09-25 20:13:21 -0300

    refactor: update prompt with detailed instructions and examples

--- internal/agents/agent.interface.go
+++ internal/agents/agent.interface.go
@@ -4,7 +4,17 @@ import (
 	c "github.com/eduardongomes/gcai/internal/config"
 )
 
-const Prompt = "You are a senior software engineer. Analyze the given git diff and output only a single-line, professional conventional commit message following the Conventional Commits specification (type, scope if applicable, and short description): \n"
+const Prompt = `You are a senior software engineer. Analyze the given git diff and output only a single-line, professional conventional commit message following the Conventional Commits specification. Ensure the type and scope are in lowercase, and the message is clear and concise.
+
+Structure:
+	- type
+	- message
+
+Examples:
+- feat: implement clas Login to handler member login
+- feat: add internal link suggestion
+- fix: handle 404 errors for user profiles
+- refactor: abstract parsing logic into Parser class ` + "\n"
 
 type AgentMethods interface {
 	GetCommit(c c.ConfigMethods) (string, error)
