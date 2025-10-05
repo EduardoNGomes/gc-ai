c1bfdb34fb2d70ab8df6fe6e94a1b35c067d6574
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-10-05 00:31:41 -0300

    feat: add edit commit and alter config flags

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,30 @@ import (
 	"bufio"
 	"flag"
 	"os"
+	"strings"
 
 	"github.com/eduardongomes/gcai/internal/agents"
 	"github.com/eduardongomes/gcai/internal/cli"
 	"github.com/eduardongomes/gcai/internal/config"
+	"github.com/eduardongomes/gcai/internal/flags"
 )
 
 func main() {
 	openConfig := flag.Bool("config", false, "Enable new config keys")
+	editCommit := flag.Bool("e", false, "Edit Commit Message")
+
+	alterEditConfig := flag.String("edit", "", "Alter Configuration to edit commits")
 
 	flag.Parse()
+
+	var alterConfig *bool
+
+	if *alterEditConfig != "" {
+		val := strings.ToLower(*alterEditConfig) == "true"
+		alterConfig = &val
+	} else {
+		alterConfig = nil
+	}
 
 	reader := bufio.NewReader(os.Stdin)
 
@@ -21,5 +35,11 @@ func main() {
 	cli := cli.NewCLI()
 	geminiAgent := agents.NewGeminiAgent()
 
-	cli.Run(*openConfig, conf, geminiAgent, reader)
+	flags := flags.Flags{
+		OpenConfig:      *openConfig,
+		EditCommit:      *editCommit,
+		AlterEditConfig: alterConfig,
+	}
+
+	cli.Run(flags, conf, geminiAgent, reader)
 }
