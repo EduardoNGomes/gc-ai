e1e00f347600fa144c2ba328d55e1025699f39a7
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-10-05 00:03:28 -0300

    refactor: update cli run signature to use flags struct

--- internal/cli/main_test.go
+++ internal/cli/main_test.go
@@ -2,25 +2,26 @@ package cli
 
 import (
 	"bytes"
-	"crypto/md5"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"os"
 	"testing"
 
-	"github.com/eduardongomes/gcai/errs"
 	"github.com/eduardongomes/gcai/internal/agents"
 	c "github.com/eduardongomes/gcai/internal/config"
+	"github.com/eduardongomes/gcai/internal/flags"
 )
 
 func TestCLI(t *testing.T) {
+
+	flags := flags.Flags{
+		OpenConfig:      false,
+		EditCommit:      false,
+		AlterEditConfig: nil,
+	}
 	t.Run("Should call config method on start method when config is empty", func(t *testing.T) {
 		confSpy := c.NewConfSpy()
 		cli := NewCLI()
 		agent := agents.NewMockAgent()
 
-		cli.Run(false, confSpy, agent, &bytes.Buffer{})
+		cli.Run(flags, confSpy, agent, &bytes.Buffer{})
 
 		expect := true
 		result := confSpy.IsEmptyCalled
@@ -35,7 +36,7 @@ func TestCLI(t *testing.T) {
 		confSpy := c.NewConfSpy()
 
 		agent := agents.NewMockAgent()
-		cli.Run(false, confSpy, agent, &bytes.Buffer{})
+		cli.Run(flags, confSpy, agent, &bytes.Buffer{})
 		expect := true
 		result := confSpy.IsConfigKeyCalled
 
@@ -43,46 +44,4 @@ func TestCLI(t *testing.T) {
 			t.Errorf("Receive: '%v', Expect: '%v'", result, expect)
 		}
 	})
-}
-
-func checkAssert(t *testing.T, r, e string) {
-	t.Helper()
-
-	if e != r {
-		t.Errorf("Receive: '%s', Expect: '%s'", r, e)
-	}
-}
-
-func createTestPath(t *testing.T) string {
-	t.Helper()
-
-	hash := md5.Sum([]byte(t.Name()))
-	id := hex.EncodeToString(hash[:8])
-
-	return fmt.Sprintf("./.config-test-%s.json", id)
-}
-
-func createTestFileWithEnvs(t *testing.T, p, openai, gemini string) {
-	f, err := os.Create(p)
-	if err != nil {
-		t.Fatalf(errs.CannotOpenFileErr+" -> %v", err)
-	}
-	defer f.Close()
-
-	data := struct {
-		GeminiKey string
-		OpenAIKey string
-	}{
-		GeminiKey: gemini,
-		OpenAIKey: openai,
-	}
-
-	dataByte, err := json.Marshal(data)
-	if err != nil {
-		t.Fatalf(errs.ErrorOnConvertDataToByte+" -> %v", err)
-	}
-
-	if _, err = f.Write(dataByte); err != nil {
-		t.Fatalf(errs.ErrorOnWriteFileConfig+" -> %v", err)
-	}
 }
