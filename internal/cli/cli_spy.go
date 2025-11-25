50c4941d3171fc92ffd0a259bbd6aae659b6d187
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-11-25 17:59:57 -0300

    fix: pass agents to ConfigKey method

--- internal/cli/cli_spy.go
+++ internal/cli/cli_spy.go
@@ -6,6 +6,7 @@ import (
 
 	c "github.com/eduardongomes/gcai/internal/config"
 	f "github.com/eduardongomes/gcai/internal/flags"
+	"github.com/eduardongomes/gcai/internal/providers"
 )
 
 type CLISpy struct {
@@ -18,8 +19,9 @@ func (cli *CLISpy) Run(f f.Flags, config c.ConfigMethods, reader io.Reader) {
 		log.Fatal(err)
 	}
 
+	agents := providers.NewSelectAgentSpy()
 	if config.IsEmpty() || f.OpenConfig {
-		config.ConfigKey(reader)
+		config.ConfigKey(reader, agents)
 	}
 
 }
