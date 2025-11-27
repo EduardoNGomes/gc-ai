81406b874eb9f4369addba609b6c19b489b05d1d
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-11-27 18:06:25 -0300

    test: add tests for default prompt functionality

---
+++ internal/prompt/default-prompt_test.go
@@ -0,0 +1,45 @@
+package prompt
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDefaultPrompt(t *testing.T) {
+	result := NewDefaultPrompt().GetIntroduction()
+	t.Run("[GetIntroduction] Should return default introduction", func(t *testing.T) {
+		expected := INTRODUCTION
+		checkAssertString(t, result, expected)
+	})
+	t.Run("[GetStructure] Should return default structure", func(t *testing.T) {
+		result := NewDefaultPrompt().GetStructure()
+		expected := STRUCTURE
+		checkAssertString(t, result, expected)
+	})
+	t.Run("[GetRules] Should return default rules", func(t *testing.T) {
+		result := NewDefaultPrompt().GetRules()
+		expected := RULES
+		checkAssertArray(t, result, expected)
+	})
+	t.Run("[GetExamples] Should return default examples", func(t *testing.T) {
+		result := NewDefaultPrompt().GetExamples()
+		expected := EXAMPLES
+		checkAssertArray(t, result, expected)
+	})
+}
+
+func checkAssertString(t *testing.T, result, expect string) {
+	t.Helper()
+
+	if result != expect {
+		t.Errorf("Wrong values, expect -> %s\n receive -> %s\n", expect, result)
+	}
+}
+
+func checkAssertArray(t *testing.T, result, expect []string) {
+	t.Helper()
+
+	if !reflect.DeepEqual(result, expect) {
+		t.Errorf("Wrong values, expect -> %s\n receive -> %s\n", expect, result)
+	}
+}
