8eddcd88c63401bdea46b1057c261a5946430a9a
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-11-28 12:15:32 -0300

    feat: add prompt interface methods and types

--- internal/prompt/prompt.go
+++ internal/prompt/prompt.go
@@ -1,8 +1,24 @@
 package prompt
 
+type PromptType string
+
+const (
+	DEFAULT PromptType = "DEFAULT"
+	CUSTOM  PromptType = "CUSTOM"
+)
+
+type PromptJSON struct {
+	Introduction string   `json:"introduction"`
+	Rules        []string `json:"rules"`
+	Structure    string   `json:"structure"`
+	Examples     []string `json:"examples"`
+}
+
 type Prompt interface {
 	GetIntroduction() string
 	GetRules() []string
 	GetStructure() string
 	GetExamples() []string
+	ConvertToJSON() PromptJSON
+	ConvertToPromptString() string
 }
