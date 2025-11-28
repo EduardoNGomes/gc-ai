24be6f44c2537c78cfe35ab58c68cbbe94699d8e
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-11-28 12:07:52 -0300

    refactor: update agent interface to use direct config import

--- internal/agents/agent.interface.go
+++ internal/agents/agent.interface.go
@@ -1,25 +1,9 @@
 package agents
 
-import (
-	c "github.com/eduardongomes/gcai/internal/config"
-)
-
-const Prompt = `You are a senior software engineer. Analyze the given git diff and output only a single-line, professional conventional commit message following the Conventional Commits specification. Ensure the type and scope are in lowercase, and the message is clear and concise.
-
-Structure:
-	- type
-	- message
-
-- No use SCOPE between parentheses
-
-Examples:
-- feat: implement clas Login to handler member login
-- feat: add internal link suggestion
-- fix: handle 404 errors for user profiles
-- refactor: abstract parsing logic into Parser class ` + "\n"
+import "github.com/eduardongomes/gcai/internal/config"
 
 type AgentMethods interface {
-	GetCommit(c c.ConfigMethods) (string, error)
+	GetCommit(c config.ConfigMethods) (string, error)
 	Edit(v string) (string, error)
 	GetDiff() (string, error)
 	MakeCommit(msg string) error
