b694066487043cef3d7d908b8fd49c0d207ca8a6
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-11-30 20:09:21 -0300

    refactor: abstract menu interface and return type

--- internal/providers/menu.go
+++ internal/providers/menu.go
@@ -1,40 +1,11 @@
 package providers
 
-import "github.com/manifoldco/promptui"
+type MenuReturnOption struct {
+	Position int
+	Result   string
+}
 
 type Menu interface {
 	AddItem(label, value string)
-	Display() string
-}
-
-type ShellMenu struct {
-	title string
-	items []string
-}
-
-func NewShellMenu(title string) *ShellMenu {
-	return &ShellMenu{
-		title: title,
-		items: []string{},
-	}
-}
-
-func (m *ShellMenu) AddItem(label, value string) {
-	m.items = append(m.items, value)
-}
-
-func (m *ShellMenu) Display() string {
-	prompt := promptui.Select{
-		HideHelp: true,
-		Label:    m.title,
-		Items:    m.items,
-		Size:     3,
-	}
-
-	_, result, err := prompt.Run()
-	if err != nil {
-		return ""
-	}
-
-	return result
+	Display() (*MenuReturnOption, error)
 }
