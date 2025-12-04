1b7b25189a52b840af48ae9263d2b0d76c2aa3ed
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-03 21:41:31 -0300

    refactor: adjust MockMenu.Display to return *MenuReturnOption, error

--- internal/providers/menu_mock.go
+++ internal/providers/menu_mock.go
@@ -9,6 +9,6 @@ func (m *MockMenu) AddItem(label, value string) {
 	m.items = append(m.items, value)
 }
 
-func (m *MockMenu) Display() string {
-	return m.toReturn
+func (m *MockMenu) Display() (*MenuReturnOption, error) {
+	return &MenuReturnOption{Result: m.toReturn, Position: 0}, nil
 }
