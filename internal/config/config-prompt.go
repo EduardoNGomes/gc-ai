44aabf742edc95c7d60a6b487c6320d0fe38a7c9
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-06 17:55:18 -0300

    feat: add config prompt selector and factory

---
+++ internal/config/config-prompt.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/eduardongomes/gcai/internal/prompt"
+	"github.com/eduardongomes/gcai/internal/providers"
+)
+
+type MenuPromptSelector interface {
+	SelectPromptType() (prompt.PromptType, error)
+}
+
+type ProdMenuSelector struct{}
+
+func (ProdMenuSelector) SelectPromptType() (prompt.PromptType, error) {
+	menu := providers.NewMenuCustomPrompt("Choice your prompt type")
+	return prompt.NewMenuPromptOptions(menu)
+}
+
+type CustomPromptFactory interface {
+	New(dto prompt.CustomPromptDTO) (prompt.Prompt, error)
+}
