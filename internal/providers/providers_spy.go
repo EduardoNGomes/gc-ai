8e1a231298dba47a946dace6653a59b5dca72768
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-07 20:40:44 -0300

    fix: return nil error from SelectAgentSpy.SelectedOption

--- internal/providers/providers_spy.go
+++ internal/providers/providers_spy.go
@@ -8,8 +8,8 @@ func (s *SelectAgentSpy) SetAgent(p Provider) {
 	s.option = p
 }
 
-func (s *SelectAgentSpy) SelectedOption() Provider {
-	return s.option
+func (s *SelectAgentSpy) SelectedOption() (Provider, error) {
+	return s.option, nil
 }
 
 func NewSelectAgentSpy() *SelectAgentSpy {
