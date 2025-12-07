73ba3a47144cd8dc9f6e8617921488363393cb9c
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-06 23:36:33 -0300

    feat: add menu writer implementation

---
+++ internal/menu/menu-writer.go
@@ -0,0 +1,26 @@
+package providers
+
+import "github.com/manifoldco/promptui"
+
+type MenuWriter interface {
+	Run(label string) (string, error)
+}
+
+type ProdMenuWriter struct{}
+
+func (m *ProdMenuWriter) Run(label string) (string, error) {
+	prompt := promptui.Prompt{
+		Label: label,
+	}
+	line, err := prompt.Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	return line, nil
+}
+
+func NewProdMenuWriter() *ProdMenuWriter {
+	return &ProdMenuWriter{}
+}
