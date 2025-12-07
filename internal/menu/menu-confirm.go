286c07e8c6923c1cc7867ecb74526c77a8d0f936
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-06 23:36:50 -0300

    feat: add prod menu confirm provider

---
+++ internal/menu/menu-confirm.go
@@ -0,0 +1,27 @@
+package providers
+
+import (
+	"github.com/manifoldco/promptui"
+)
+
+type MenuConfirm interface {
+	Run(label string) error
+}
+
+type ProdMenuConfirm struct{}
+
+func (m *ProdMenuConfirm) Run(label string) error {
+	promptConfirm := promptui.Prompt{
+		Label:     label,
+		IsConfirm: true,
+	}
+
+	if _, err := promptConfirm.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func NewProdMenuConfirm() *ProdMenuConfirm {
+	return &ProdMenuConfirm{}
+}
