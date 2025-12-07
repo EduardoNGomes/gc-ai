23ac6abcf7c29132285eed7e39cf0ac683228dda
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-06 23:37:00 -0300

    feat: add prod menu editable provider

---
+++ internal/menu/menu-edit.go
@@ -0,0 +1,30 @@
+package providers
+
+import (
+	"github.com/manifoldco/promptui"
+)
+
+type MenuEditable interface {
+	Run(label, defaultValue string) (string, error)
+}
+
+type ProdMenuEditable struct{}
+
+func (m *ProdMenuEditable) Run(label, defaultValue string) (string, error) {
+	promptConfirm := promptui.Prompt{
+		Label:     label,
+		Default:   defaultValue,
+		AllowEdit: true,
+	}
+
+	line, err := promptConfirm.Run()
+	if err != nil {
+		return "", err
+	}
+
+	return line, nil
+}
+
+func NewProdMenuEditable() *ProdMenuEditable {
+	return &ProdMenuEditable{}
+}
